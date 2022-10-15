4cf5afb687174b471d8bb1d24ee0eee4a2ca6320
Graham Ollis <plicease@cpan.org>
2022-10-15 14:28:02 -0600

    initial implementation

---
+++ README.md
@@ -0,0 +1,14 @@
+# Acme::Color::Rust ![static](https://github.com/PerlFFI/Acme-Color-Rust/workflows/static/badge.svg) ![linux](https://github.com/PerlFFI/Acme-Color-Rust/workflows/linux/badge.svg)
+
+Color example class using Rust + FFI
+
+# AUTHOR
+
+Graham Ollis <plicease@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2022 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Acme/Color/Rust.pm
+++ lib/Acme/Color/Rust.pm
@@ -1,12 +1,29 @@
-package Acme::Color::Rust;
+package Acme::Color::Rust {
 
-use strict;
-use warnings;
-use 5.008004;
+  use warnings;
+  use 5.020;
+  use experimental qw( signatures );
+  use FFI::Platypus 2.00;
 
 # ABSTRACT: Color example class using Rust + FFI
 # VERSION
 
+  my $ffi = FFI::Platypus->new( api => 2, lang => 'Rust' );
+  $ffi->bundle;
+  $ffi->mangler(sub ($name) { "color_$name" });
+  $ffi->type('object(Acme::Color::Rust,u32)' => 'color');
+
+  $ffi->attach( new => ['string','u8','u8','u8'] => 'u32' => sub ($xsub, $class, $name, $r, $g, $b) {
+    my $index = $xsub->($name, $r, $g, $b);
+    bless \$index, $class;
+  });
+
+  $ffi->attach( name  => ['color'] => 'string' );
+  $ffi->attach( red   => ['color'] => 'u8'     );
+  $ffi->attach( green => ['color'] => 'u8'     );
+  $ffi->attach( blue  => ['color'] => 'u8'     );
+
+  $ffi->attach( DESTROY => ['color'] );
+}
+
 1;
-
-

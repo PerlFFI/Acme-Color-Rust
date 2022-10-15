06be8d9df1e1f3c31bc53d46054460da0309fd7e
Graham Ollis <plicease@cpan.org>
2022-10-15 14:52:05 -0600

    initial implementation

--- lib/Acme/Color/Rust.pm
+++ lib/Acme/Color/Rust.pm
@@ -8,15 +8,70 @@ package Acme::Color::Rust {
 # ABSTRACT: Color example class using Rust + FFI
 # VERSION
 
+=head1 SYNOPSIS
+
+ use Acme::Color::Rust;
+
+ my $color = Acme::Color::Rust->new("red", 0xff, 0x00, 0x00);
+ say "the color is ", $color->name;
+ say "with red ", $color->red, " green ", $color->green, " and blue ", $color->blue;
+
+=head1 DESCRIPTION
+
+This class is a very simple RGB color class.  It is implemented using Rust.  It is 
+mostly intended as a test for extending perl using Rust concept.
+
+=cut
+
   my $ffi = FFI::Platypus->new( api => 2, lang => 'Rust' );
   $ffi->bundle;
   $ffi->mangler(sub ($name) { "color_$name" });
   $ffi->type('object(Acme::Color::Rust,u32)' => 'color');
 
-  $ffi->attach( new => ['string','u8','u8','u8'] => 'u32' => sub ($xsub, $class, $name, $r, $g, $b) {
-    my $index = $xsub->($name, $r, $g, $b);
+=head1 CONSTRUCTOR
+
+=head2 new
+
+ my $color = Acme::Color::Rust->new($name, $red, $green, $blue);
+
+This create a new instance of L<Acme::Color::Rust>.  The name, red, green and blue 
+values are passed in and the new instance is returned.  The color values should be 8
+bit unsigned values (that is 0-255).
+
+=cut
+
+  $ffi->attach( new => ['string','u8','u8','u8'] => 'u32' => sub ($xsub, $class, $name, $r, $g, $bl) {
+    my $index = $xsub->($name, $r, $g, $bl);
     bless \$index, $class;
   });
+
+=head1 METHODS
+
+=head2 name
+
+ my $name = $color->name;
+
+The name of the color.
+
+=head2 red
+
+ my $red = $color->red;
+
+The red component of the color.  This should be an 8-bit unsigned value (that is 0-255).
+
+=head2 green
+
+ my $green = $color->green;
+
+The green component of the color.  This should be an 8-bit unsigned value (that is 0-255).
+
+=head2 blue
+
+ my $blue = $color->blue;
+
+The blue component of the color.  This should be an 8-bit unsigned value (that is 0-255).
+
+=cut
 
   $ffi->attach( name  => ['color'] => 'string' );
   $ffi->attach( red   => ['color'] => 'u8'     );
